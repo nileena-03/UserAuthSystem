45fa701909c32ca367bd15af47654dd5909e3d15
nileena_03 <nileenasanthosh03@gmail.com>
2025-11-10 11:19:12 +0530

    all changes

--- members/views.py
+++ members/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return render(request,'register.html')
+
+def loginUser(request):
+    return render(request,'login.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
