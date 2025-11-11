a7cc922677c66ee827e9225fe991b1e6591b370f
nileena_03 <nileenasanthosh03@gmail.com>
2025-11-11 11:45:30 +0530

    all changes

--- members/urls.py
+++ members/urls.py
@@ -5,5 +5,7 @@ from .import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.loginUser, name='login'),
-    path('dashboard/', views.dashboard , name='dashboard')
+    path('dashboard/', views.dashboard , name='dashboard'),
+    path('logout/', views.logoutUser, name='logout')
+
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -14,7 +16,21 @@ def register(request):
     return render(request,'register.html')
 
 def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request,user)
+            return redirect ('dashboard')
+        else:
+            messages.error(request,"Invalid credentials")
     return render(request,'login.html')
 
+@login_required
 def dashboard(request):
     return render(request,'dashboard.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
